75319a5a6feb7812ce60959ee1e6557273f8071c
indegame <indegamedev@gmail.com>
2018-08-05 17:33:00 +1000

    Creation of the non-functional add task page

--- lib/main.dart
+++ lib/main.dart
@@ -3,107 +3,121 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  final name = "Tasker";
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(
+        title: name,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(name),
+              actions: <Widget>[
+                new IconButton(
+                    icon: const Icon(Icons.list), onPressed: _pushAddTasks)
+              ],
+            ),
+            //body: new Center(child: TaskView())),
+            body: Container(margin: EdgeInsets.all(30.0), child: TaskView())));
   }
+
+  void _pushAddTasks() {}
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TaskView extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  State createState() => new TaskViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TaskViewState extends State<TaskView> {
+  static List<Priority> priorities = <Priority>[const Priority("Low", 1), const Priority("Medium", 2), const Priority("High", 3)];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _formKey = GlobalKey<FormState>();
+  var _selectedPriority = priorities[1];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Form(
+        key: _formKey,
+        child: Column(
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0, top: 20.0),
+              child: Column(
+                children: <Widget>[
+                  Text("Task Name"),
+                  TextFormField(
+                    // The validator receives the text the user has typed in
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                    },
+                  ),
+                ],
+              )),
+            Container(
+                margin: EdgeInsets.only(bottom: 20.0, top: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Description"),
+                    TextFormField(
+                      // The validator receives the text the user has typed in
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                      },
+                    ),
+                  ],
+            )),
+            Container(
+              margin: EdgeInsets.only(bottom: 20.0, top: 20.0),
+              child: Column(
+                children: <Widget>[
+                Text("Priority"),
+                new DropdownButton<Priority>(
+                  hint: new Text("Select a priority"),
+                  value: _selectedPriority,
+                  onChanged: (Priority newValue) {
+                    setState(() {
+                      _selectedPriority = newValue;
+                    });
+                  },
+                  items: priorities.map((Priority priority) {
+                    print(priorities[2]);
+                    return new DropdownMenuItem<Priority>(
+                      value: priority,
+                      child: new Text(
+                        priority.name,
+                        style: new TextStyle(color: Colors.black),
+                      ),
+                    );
+                  }).toList(),
+                )],
+              )
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Flexible(child:
+              Align(
+                child: RaisedButton(child:
+                Text("Add"),
+                  color: Colors.lightBlue,
+                  onPressed: _onAdd,
+                  textColor: Colors.white,),
+                alignment: Alignment.bottomCenter,
+              )
+            )
           ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        ));
   }
+
+  void _onAdd() {}
+}
+
+class Priority{
+  const Priority(this.name, this.level);
+
+  final String name;
+  final int level;
 }
